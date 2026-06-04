0ca4360aba6db97f7ea28956a07b06ff11c9d550
Nishit Jha <jhanishitback@gmail.com>
2026-06-04 22:11:36 +0530

    init

---
+++ app/api/validate/route.ts
@@ -0,0 +1,2 @@
+// API endpoint which validates a given guess against the word of the day
+// Returns status of the guess only 
---
+++ app/api/word/route.ts
@@ -0,0 +1,3 @@
+// Endpoint for fetching the day's word
+// Returns the word only. 
+// Even if hints are added later, they must only be returned on subsequent requests made to some other endpoint
